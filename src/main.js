a68c8036f2fad2e07adef8c7480a1da92d22a076
sThig <scott.thigpen@gmail.com>
2017-04-14 21:50:27 -0500

    I hate sass

--- src/main.js
+++ src/main.js
@@ -29,6 +29,7 @@ function editSwitcher() {
   const textname = document.createElement('span');
   textname.setAttribute("contenteditable", "false");
   textname.id = "editName";
+  textname.setAttribute("class", "editli")
   return textname;
 }
 
@@ -226,10 +227,10 @@ function generateMiddleNickname() {
       let namedteam = generateName();
       elem.className = 'hide';
 
-      document.getElementById('captain').innerHTML += (`<h2 class="test">Team ${namedteam}!</h2>`);
+      // document.getElementById('captain').innerHTML += (`<h2 class="test">Team ${namedteam}!</h2>`);
       document.getElementById('captain').innerHTML += (`<p class="congrats">Congrats! You're <b style="color: black;">${namedteam}</b> Now go win the touchdown and the free throws! SPORTS!</p>`);
-      document.getElementById('captain').innerHTML += (`<p class="captain-subtext">Your captain is</p><h2 class="thecaptain">\u2B51 \u2B51 ${captain} \u2B51 \u2B51 </h2>`);
-      document.getElementById('captain').innerHTML += (`<p class="captain-subtext">Your co-captain is</p><h2 class="thecaptain">\u2B51 ${id} \u2B51 </h2>`);
+      document.getElementById('captain').innerHTML += (`<p class="captain-subtext">Your captain is</p><h2 class="thecaptain">${captain}</h2>`);
+      document.getElementById('captain').innerHTML += (`<p class="captain-subtext">Your co-captain is</p><h2 class="thecaptain">${id}</h2>`);
       teammates.forEach(function(teammates) {
         document.getElementById('teammateys').innerHTML += (`<li class="forcefont">${teammates}</li>`);
       });
